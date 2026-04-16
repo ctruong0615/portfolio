243e546fd33e543ecc11b761be6d2969f4fcebc7
Calvin Truong <cat020@ucsd.edu>
2026-04-15 21:37:37 -0700

    FINAL
    lol

--- global.js
+++ global.js
@@ -5,10 +5,10 @@ export function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'contact/', title: 'Contact' },
-  { url: 'resume/', title: 'Resume' },
+  { url: 'index.html', title: 'Home' },
+  { url: 'projects/index.html', title: 'Projects' },
+  { url: 'contact/index.html', title: 'Contact' },
+  { url: 'resume/index.html', title: 'Resume' },
   { url: 'https://github.com/ctruong0615', title: 'GitHub' },
 ];
 
